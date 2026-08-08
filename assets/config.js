522b423e63a38e37669744d15f2bf9c8a5883f42
AKELBA <office@akelba.jp>
2026-08-08 17:01:27 +0900

    Activate GA4 measurement

--- assets/config.js
+++ assets/config.js
@@ -4,5 +4,5 @@ window.AKELBA_CONFIG = Object.freeze({
   diagnosisForm: "",
   // Google Analytics 4 の測定ID。例: G-XXXXXXXXXX
   // 空欄または形式が不正な場合、アクセス情報は送信されません。
-  ga4MeasurementId: ""
+  ga4MeasurementId: "G-EQ9L3VZHEV"
 });
