93de4a5778c0fecfde50e63fc20e38b1b8e287f7
AKELBA <office@akelba.jp>
2026-08-08 11:49:34 +0900

    Prepare GA4 measurement configuration

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,8 @@
 window.AKELBA_CONFIG = Object.freeze({
   email: "office@akelba.jp",
   instagram: "https://www.instagram.com/akelba.office/",
-  diagnosisForm: ""
+  diagnosisForm: "",
+  // Google Analytics 4 の測定ID。例: G-XXXXXXXXXX
+  // 空欄または形式が不正な場合、アクセス情報は送信されません。
+  ga4MeasurementId: ""
 });
